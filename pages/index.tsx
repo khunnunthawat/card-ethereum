9cb17356b280670a14da30b0dfe778a2911c57b4
khunnunthawat <khunnunthawat@gmail.com>
2022-07-14 01:04:03 +0700

    fix-font-weight

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className='flex items-center justify-center min-h-screen'>
-      <h1 className='text-2xl text-blue-500'>
-        Nextjs Tailwind Typescript Setup
-      </h1>
+    <div className='flex items-center justify-center min-h-screen mx-auto bg-neutral-main'>
+      <div className='text-lg text-primary-cyan'>
+        <h1 className='text-2xl text-primary-soft-blue'>
+          Nextjs Tailwind Typescript Setup
+        </h1>
+        <h4 className='font-normal'>Font Weight 300</h4>
+        <h4 className='font-medium'>Font Weight 400</h4>
+        <h4 className='font-semibold'>Font Weight 600</h4>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,28 @@ module.exports = {
   ],
   important: true,
   theme: {
+    debugScreens: { position: ['top', 'left'] },
+    screens: {
+      mobile: '375px',
+      desktop: '1440px',
+    },
     extend: {
       fontFamily: {
         sans: ['Outfit', ...defaultTheme.fontFamily.sans],
       },
     },
+    colors: {
+      primary: {
+        'soft-blue': 'hsl(215, 51%, 70%)',
+        cyan: 'hsl(178, 100%, 50%)',
+      },
+      neutral: {
+        main: 'hsl(217, 54%, 11%)',
+        card: 'hsl(216, 50%, 16%)',
+        line: 'hsl(215, 32%, 27%)',
+      },
+      white: 'hsl(0, 0%, 100%)',
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-debug-screens')],
 };
